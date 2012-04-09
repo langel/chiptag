3ccb9e602e239df1a7c000320426d2f152eae06c
Langel <knoxburry@gmail.com>
2012-04-08 19:49:08 -0700

    now reading nsf info including extensions

--- chiptag.php
+++ chiptag.php
@@ -1,26 +1,62 @@
 <?php
+/**********************************************************
+
+	C H I P T A G
+
+	a chiptune tag reader / writer in php
+
+	a proud member of the NinjaWizard family
+
+**********************************************************/
+
+
+foreach (glob(dirname(__FILE__).'/formats/*.php') as $file)	{
+	include($file);
+}
+
 
 
 class chiptag	{
+
+	function open($file)	{
+		if (!is_file($file))	{
+			$error = $file.' - file unfound. ';
+		}
+		$ext = substr($file,strrpos($file,'.')+1);
+		$class = 'chiptag_format_'.$ext;
+		if (!class_exists($class))	{
+			$error .= strtoupper($ext).' not currently supported. ';
+		}
+		if ($error=='')	{
+			return new $class($file);
+		}
+		else return new chiptag_format($error);
+	}
   
-  function Open($filename) {
-    $a = new mp3;
-    $a->filename = $filename;
-    $a->filesize = filesize($filename);
-    $a->file = fopen($a->filename,'r+b');
-    $a->FetchID3v1();
-    $a->FetchID3v2();
-    $a->FetchInfos();
-    $a->newTags = array();
-    print_r($a);
-    return $a;
-  }
-
-  function Close() {
-    fclose($this->file);
-  }
-
 }
 
+
+class chiptag_format	{
+
+	static $bitnums = array(1,2,4,8,16,32,64,128);
+
+	function __construct($errorText)	{
+		$this->report = array($errorText);
+	}
+	
+	function FetchInfo()	{
+		return $this->report;
+	}
+
+	function ReportHelp()	{
+		$posse = array('artist','title','copy');
+		foreach ($posse as $tag)	{
+			if (trim($this->$tag)!='')	{
+				$this->report[] = $tag.' : '.$this->$tag;
+			}
+		}
+	}
+
+}
 
 ?>
